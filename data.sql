81e6d566d35b606720a8c83b08f00bb2536597db
root <personne.nico@gmail.com>
2018-12-19 11:50:47 +0100

    Ex3

---
+++ data.sql
@@ -0,0 +1,3 @@
+# Nous créons une dbb nommé webDevelopment avec les options CHAr set utf8 si elle n'existe pas
+
+CREATE DATABASE IF NOT EXISTS `frameworks` CHARACTER SET utf8 COLLATE utf8_general_ci;
